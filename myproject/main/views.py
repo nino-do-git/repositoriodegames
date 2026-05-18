dac309b637cfb369020f30166ac85f8e502321cb
Marino Dias <marinomdias@gmail.com>
2026-05-18 09:18:04 -0300

    index.html criado

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -3,10 +3,8 @@ from .models import Game
 from django.http import HttpResponse
 
 def index(request):
-    all_games = Game.objects.all()
-    for game in all_games:
-        print(game.title)
-    return HttpResponse("Olá mundo! Este é o app notes de DevLife do Insper.")
+    games = Game.objects.all()
+    return render(request, 'index.html', {'games': games})
 
 def game_page(request):
     games = Game.objects.all()
