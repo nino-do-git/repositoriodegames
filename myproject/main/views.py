bd853079e1e50902f710650796df7c7574ddedcf
Nicolas <nicolaspradosaezcontreras2004@gmail.com>
2026-05-18 08:07:56 -0300

    garantir commit

--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Game
+from django.http import HttpResponse
 
 def index(request):
-    all_notes = Game.objects.all()
-    return render(request, 'game.html', {'games': all_notes})
+    all_games = Game.objects.all()
+    for game in all_games:
+        print(game.title)
+    return HttpResponse("Olá mundo! Este é o app notes de DevLife do Insper.")
+    #return render(request, 'game.html', {'games': all_notes})
